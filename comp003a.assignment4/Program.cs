0062d8c10855fecc83ed0bf83f30f5e36716ea0b
Your Name <you@example.com>
2025-02-06 14:12:04 -0800

    added the option to choose wether they want to use list or array

--- comp003a.assignment4/Program.cs
+++ comp003a.assignment4/Program.cs
@@ -4,8 +4,27 @@
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("Welcome to the inventory management");
+            Console.WriteLine("Choose your storage method: ");
+            Console.WriteLine("1. Arrays");
+            Console.WriteLine("2. Lists");
+            int choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                UseArrays();
+            }
+            else if (choice == 2)
+            {
+                UseLists();
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice.");
+            }
         }
+
+
 
 
     }
