a4630c9a36662f558ccc6d8c3124f62d1c345b12
Your Name <you@example.com>
2025-02-06 14:43:55 -0800

    added case 1

--- comp003a.assignment4/Program.cs
+++ comp003a.assignment4/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace comp003a.assignment4
+﻿using System.Transactions;
+
+namespace comp003a.assignment4
 {
     internal class Program
     {
@@ -24,7 +26,44 @@
             }
         }
 
+        static void UseArray()
+        {
+            string[] productName = new string[10];
+            int[] quantities = new int[10];
+            int count = 0;
 
+            while (true)
+            {
+                Console.WriteLine("\nMenu: ");
+                Console.WriteLine("1. Add Product");
+                Console.WriteLine("2. Update Product Quantity");
+                Console.WriteLine("3. View inventory");
+                Console.WriteLine("4. Exit");
+                int option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        if (count >= 10)
+                        {
+                            Console.WriteLine("Inventory is full. Cannot add more products");
+                            break;
+                        }
+                        Console.Write("Enter product name: ");
+                        String name = Console.ReadLine();
+                        Console.Write("Enter product quantity");
+                        int quantity = int.Parse(Console.ReadLine());
+
+                        productName[count] = name;
+                        quantities[count] = quantity;
+                        count++;
+                        Console.WriteLine("Product added.");
+                        break;
+
+                }
+
+            }
+        }
 
 
     }
